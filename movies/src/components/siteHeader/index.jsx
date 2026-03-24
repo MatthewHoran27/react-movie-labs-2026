39cba4c6d80e7dd07b760f5fac225349a4698941
MatthewHoran27 <20093995@mail.wit.ie>
2026-03-24 21:17:34 +0000

    CSS Update Nav

--- movies/src/components/siteHeader/index.jsx
+++ movies/src/components/siteHeader/index.jsx
@@ -3,23 +3,18 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import { useNavigate } from "react-router";
 import { styled } from '@mui/material/styles';
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 const SiteHeader = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [drawerOpen, setDrawerOpen] = useState(false);
   
   const navigate = useNavigate();
 
@@ -34,73 +29,58 @@ const SiteHeader = () => {
   ];
 
   const handleMenuSelect = (pageURL) => {
-    setAnchorEl(null);
+    setDrawerOpen(false);
     navigate(pageURL);
   };
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleMenuOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleMenuClose = () => {
+    setDrawerOpen(false);
   };
 
   return (
     <>
       <AppBar position="fixed" color="secondary">
         <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
+          <Typography variant="h4" sx={{ flexGrow: 1, fontFamily: 'PixelGame' }}>
+            MATMDB Client
           </Typography>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            All you ever wanted to know about Movies!
+          <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: 'PixelGame' }}>
+            Matthew's Awesome TMDB Client
           </Typography>
-            {isMobile ? (
-              <>
-                <IconButton
-                  aria-label="menu"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
+          <IconButton
+            aria-label="menu"
+            aria-controls="menu-drawer"
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            id="menu-drawer"
+            anchor="right"
+            open={drawerOpen}
+            onClose={handleMenuClose}
+          >
+            <List sx={{ width: 250 }}>
+              {menuOptions.map((opt) => (
+                <ListItem
+                  button
+                  key={opt.label}
+                  onClick={() => handleMenuSelect(opt.path)}
                 >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={open}
-                  onClose={() => setAnchorEl(null)}
-                >
-                  {menuOptions.map((opt) => (
-                    <MenuItem
-                      key={opt.label}
-                      onClick={() => handleMenuSelect(opt.path)}
-                    >
-                      {opt.label}
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </>
-            ) : (
-              <>
-                {menuOptions.map((opt) => (
-                  <Button
-                    key={opt.label}
-                    color="inherit"
-                    onClick={() => handleMenuSelect(opt.path)}
-                  >
-                    {opt.label}
-                  </Button>
-                ))}
-              </>
-            )}
+                  <ListItemText 
+                    primary={opt.label} 
+                    primaryTypographyProps={{ sx: { fontFamily: 'PixelGame' } }}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
         </Toolbar>
       </AppBar>
       <Offset />
